b51b2422ec14f249176ac4b93a7258bc40261aef
Cynthia Inga <candyinga67@gmail.com>
2022-02-03 21:24:11 +0200

    Create a array for the element of featured speakers

---
+++ JS/speakers.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+      id: 1,
+      name: 'Gloria Tumushabe',
+      images: {
+        img: './images/Gloria.jpeg',
+        alt: 'speaker-1',
+      },
+      work: 'Technical Interview Prep.',
+      about: 'Gloria Tumushabe is the Founder and an instructor at Afro Fem Coders.She\'s a UC Berkeley computer science Master student from Uganda.',
+    },
+    {
+      id: 2,
+      name: 'Lavanya Vijayan',
+      images: {
+        img: './images/Lavanya.jpg',
+        alt: 'speaker-2',
+      },
+      work: 'Scratch and Python',
+      about: 'Lavanya is lead Instructor at Afro Fem Coders, a Master\'s student in Data Science ar UC Berkeley, a coding instructor at the coder school Berkeley, and author of programming courses on LinkedIn Learning.',
+    },
+    {
+      id: 3,
+      name: 'Olivia Hsu',
+      images: {
+        img: './images/Olivia.jpeg',
+        alt: 'speaker-3',
+      },
+      work: 'Machine Learning',
+      about: 'Olivia is an Instructor and advisor for Afro Fem Coders. She is currently a second year PhD student at Stanford University studdying Computer Science.',
+    },
+    {
+      id: 4,
+      name: 'Concepta Njolima',
+      images: {
+        img: './images/Concepta.jpeg',
+        alt: 'speaker-4',
+      },
+      work: 'Web Development',
+      about: 'Concepta is a sophomore Computer Science and Mathematics double-major at Berea College from Uganda and an Instructor at Afro Fem Coders.',
+    },
+    {
+      id: 5,
+      name: 'Tensae Kebede',
+      images: {
+        img: './images/Tensae.jpeg',
+        alt: 'speaker-5',
+      },
+      work: 'Python and Data Analytics',
+      about: 'Tensae is currently interning at Intel as a System Validation Engineer. She working on a software tool to ensure the quality, compatibility, and data integrity of driver.',
+    },
+    {
+      id: 6,
+      name: 'Tina Taleb',
+      images: {
+        img: './images/Tina.jpeg',
+        alt: 'speaker-6',
+      },
+      work: 'Design Innovation',
+      about: 'Tina graduated from UC Berkeley with Bachelor\'s in Electrical Engineering and Computer Science and Design Innovation Certificate. She\'s passionate about merging technology with art and using it in education and to improve people\'s lives.',
+    },
+  ];
