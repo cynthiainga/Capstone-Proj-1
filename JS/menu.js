acd4d177ecc9753a8e5bce86e209ada35b184d5a
Cynthia Inga <candyinga67@gmail.com>
2022-02-03 21:08:00 +0200

    Add menu.js file and declare all the variables

---
+++ JS/menu.js
@@ -0,0 +1,5 @@
+const html = document.getElementsByTagName('html')[0];
+const showBtn = document.querySelector('nav .show-menu-btn');
+const closeBtn = document.querySelector('nav .close-menu-btn');
+const navMenu = document.querySelector('nav .nav-menu');
+const navLinks = document.querySelectorAll('nav .nav-menu li');
