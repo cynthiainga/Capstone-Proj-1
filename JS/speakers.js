d89f82d0e335631896ea6de8dd37f2cf658f0d93
Cynthia Inga <candyinga67@gmail.com>
2022-02-03 21:33:02 +0200

    Implementing the Flex card dynamically

--- JS/speakers.js
+++ JS/speakers.js
@@ -60,3 +60,59 @@ const data = [
       about: 'Tina graduated from UC Berkeley with Bachelor\'s in Electrical Engineering and Computer Science and Design Innovation Certificate. She\'s passionate about merging technology with art and using it in education and to improve people\'s lives.',
     },
   ];
+
+  const teamContainer = document.querySelector('#Team .teams');
+
+  function createCard(dataInfo) {
+    const li = document.createElement('li');
+    li.className = 'card d-flex';
+    li.innerHTML = `<div class="card-img">
+        <img src="${dataInfo.images.img}" alt="${dataInfo.images.alt}">
+      </div>
+      <div class="card-body">
+        <h3>${dataInfo.name}</h3>
+        <h4>${dataInfo.work}</h4>
+        <p>${dataInfo.about}</p>
+      </div>`;
+    return li;
+  }
+  
+  data.forEach((info) => {
+    const card = createCard(info);
+    teamContainer.appendChild(card);
+  });
+  
+  const teamList = document.querySelectorAll('#Team .teams li');
+  const moreBtn = document.querySelector('#Team button');
+  const btnSpan = document.querySelector('#Team button span');
+  const btnIcon = document.querySelector('#Team button i');
+  
+  let state = false;
+  
+  function displayMore() {
+    if (!state) {
+      teamList.forEach((team) => {
+        team.style.display = 'flex';
+      });
+      btnSpan.textContent = 'LESS';
+      btnIcon.classList.remove('fa', 'fa-chevron-down');
+      btnIcon.classList.add('fa', 'fa-chevron-up');
+  
+      state = true;
+    } else {
+      teamList.forEach((team) => {
+        team.style.display = 'none';
+      });
+      teamList[0].style.display = 'flex';
+      teamList[1].style.display = 'flex';
+  
+      btnSpan.textContent = 'MORE';
+      btnIcon.classList.remove('fa', 'fa-chevron-up');
+      btnIcon.classList.add('fa', 'fa-chevron-down');
+  
+      state = false;
+    }
+  }
+  
+  moreBtn.addEventListener('click', displayMore);
+  
